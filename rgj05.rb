4ee64a04276162454e190bac3b7702828d1a1b21
Kenneth Pullen <kenneth.pullen@gmail.com>
2011-01-09 01:22:42 -0800

    Initial commit. All gameplay should be complete by now, which is good, because I need sleep. Still to do: animate bird; draw / animate tourist; animate hugging / high fiving.

---
+++ rgj05.rb
@@ -0,0 +1,225 @@
+#!/usr/bin/env ruby
+require 'gosu'
+include Gosu
+TIMELINE_HEIGHT = 50
+ACTION_TYPES = [:hug, :high_five, :love_beam]
+class RGJ05 < Window
+	attr_accessor :score
+	def self.window
+		ObjectSpace.each_object(RGJ05).first
+	end
+	%w(width height).each {|m| define_singleton_method(m) {window.send m}}
+	def initialize
+		super 320, 240, false
+		@actions = []
+		@last_action_pushed = 0
+		@reticle = Image.new self, 'reticle.png', false
+		@bear = Bear.new
+		@action_queue = 100.times.inject([]) {|m, _| m.push ACTION_TYPES.sample}
+		@font = Font.new self, 'Retroville NC', 14
+		@small = Font.new self, 'Retroville NC', 10
+		@score = 0
+		@delay = 2000
+		@speed = 2000.0
+		@sky_blue = Color.new 255, 173, 216, 230
+		@dodger_blue = Color.new 255, 30, 144, 255
+		@lawn_green = Color.new 255, 124, 252, 0
+		@misses = 0
+	end
+	def update
+		@actions.each(&:update)
+		@actions.delete_if {|a| a.x <= 0}
+		if milliseconds - @last_action_pushed > @delay && @bear.up_to_speed
+			@actions.push Action.new @speed, @action_queue.shift
+			@last_action_pushed = milliseconds
+		end
+		@bear.up_to_speed = true if @bear.pushes > 9
+		@bear.update
+		@delay = 1500 and @speed = 1900.0 if @score > 1000
+		@delay = 1000 and @speed = 1800.0 if @score > 2000
+		if @misses >= 5
+			@bear.up_to_speed = false
+			@misses = 0
+			@bear.pushes = 0
+		end
+	end
+	def draw
+		draw_quad(
+			0, 0, @sky_blue,
+			width, 0, @sky_blue,
+			width, height, @dodger_blue,
+			0, height, @dodger_blue)
+		draw_quad(
+			0, height / 2, @lawn_green,
+			width, height / 2, @lawn_green,
+			width, height, @lawn_green,
+			0, height, @lawn_green)
+		draw_quad(
+			0, height - TIMELINE_HEIGHT - 20, Color::GRAY,
+			width, height - TIMELINE_HEIGHT - 20, Color::GRAY,
+			width, height - TIMELINE_HEIGHT + 20, Color::GRAY,
+			0, height - TIMELINE_HEIGHT + 20, Color::GRAY)
+		unless @bear.up_to_speed
+			@font.draw 'GET UP TO SPEED!', width / 2 - @font.text_width('GET UP TO SPEED!') / 2, 50, 5
+			@font.draw @bear.pushes, width / 2 + @font.text_width('GET UP TO SPEED!') / 2 + 10, 50, 5
+		end
+		translate width / 2, height - TIMELINE_HEIGHT do
+			scale 2, 2 do
+				translate -@reticle.width / 2, -@reticle.height / 2 do
+					@reticle.draw 0, 0, 1
+				end
+			end
+		end
+		@actions.each do |a|
+			translate a.x, RGJ05.height - TIMELINE_HEIGHT do
+				scale 1.5, 1.5 do
+					translate -a.image.width / 2, -a.image.height / 2 do
+						if a.attempted
+							if a.success
+								draw_quad(
+									0, 0, Color::GREEN,
+									20, 0, Color::GREEN,
+									20, 20, Color::GREEN,
+									0, 20, Color::GREEN)
+							else
+								draw_quad(
+									0, 0, Color::RED,
+									20, 0, Color::RED,
+									20, 20, Color::RED,
+									0, 20, Color::RED)
+							end
+						end
+						a.image.draw 0, 0, 0
+						text = a.action_type.to_s.tr('_', ' ')
+						@small.draw text, -@small.text_width(text) / 2 + a.image.width / 2, 5, 0
+					end
+				end
+			end
+		end
+		translate width / 2, height / 2 do
+			scale 2, 2 do
+				translate -@bear.image.width / 2, -@bear.image.height / 2 do
+					@bear.image.draw 0, 0, 1
+				end
+			end
+		end
+		@small.draw 'h to hug', 10, 10, 5
+		@small.draw 'f to high five', 10, 20, 5
+		@small.draw 'z to love beam', 10, 30, 5
+		@small.draw 'alternate l and r to get up to speed!', 10, 40, 5
+		@small.draw "you have #{@score} points! Keep up the love!", 10, 50, 5 if @bear.up_to_speed
+		@small.draw "Uh-oh, you have missed #{@misses} times!", 10, 60, 5 if @misses > 0
+		action = @actions.first
+		if action && action.action_type == :love_beam && action.attempted && action.success
+			draw_line RGJ05.width / 2, RGJ05.height / 2, Color::RED, RGJ05.width - rand() * 20, 0, Color::YELLOW
+			draw_line RGJ05.width / 2, RGJ05.height / 2, Color::RED, RGJ05.width - rand() * 20, 0, Color::RED
+		end
+	end
+	def button_down id
+		if id == KbL && !@bear.up_to_speed
+			if @bear.last_foot == :right
+				@bear.last_foot = :left
+				@bear.pushes += 1
+				@bear.last_push = milliseconds
+			else
+				@bear.pushes = 0
+			end
+		end
+		if id == KbR && !@bear.up_to_speed
+			if @bear.last_foot == :left
+				@bear.last_foot = :right
+				@bear.pushes += 1
+				@bear.last_push = milliseconds
+			else
+				@bear.pushes = 0
+			end
+		end
+		if @bear.up_to_speed
+			action = @actions.detect {|a| !a.attempted}
+			if action
+				action.attempted = true
+				lower, upper = RGJ05.width / 2 - 20, RGJ05.width / 2 + 20
+				@misses += 1
+				case id
+					when KbH then
+						if action.action_type == :hug
+							if action.x < upper && action.x > lower
+								@score += 100
+								action.success = true
+								@misses = 0
+							end
+						end
+					when KbF then
+						if action.action_type == :high_five
+							if action.x < upper && action.x > lower
+								@score += 100
+								action.success = true
+								@misses = 0
+							end
+						end
+					when KbZ then
+						if action.action_type == :love_beam
+							if action.x < upper && action.x > lower
+								@score += 100
+								action.success = true
+								@misses = 0
+							end
+						end
+				end
+			end
+		end
+		close if id == KbEscape
+	end
+end
+class Action
+	attr_accessor :x, :image, :action_type, :attempted, :success
+	def initialize length, action_type
+		@start, @length, @action_type = milliseconds, length, action_type
+		@image = Image.new RGJ05.window, 'box.png', false
+		@x = RGJ05.width
+		@attempted = false
+		@success = false
+	end
+	def update
+		@x = RGJ05.width - (RGJ05.width * (milliseconds - @start) / @length)
+	end
+end
+class Bear
+	attr_accessor :up_to_speed, :last_foot, :pushes, :last_push
+	def initialize
+		@images = Image.load_tiles RGJ05.window, 'bear.png', 20, 40, false
+		@up_to_speed = false
+		@last_foot = :right
+		@pushes = 0
+		@frame = 0
+		@states = {standing: 0..0, walk_left: 1..4, walk_right: 4..7, walking: 1..7}
+		@state = :standing
+		@last_update = 0
+		@delay = 100
+		@last_push = 0
+	end
+	def image
+		@images[@frame]
+	end
+	def update
+		if @up_to_speed || milliseconds  - @last_push < 400
+			@state = :walking
+			next_frame
+		else
+			@state = :standing
+			@frame = 0
+			@last_foot = :right
+			@pushes = 0
+		end
+	end
+	def next_frame
+		animate! and @last_update = milliseconds if milliseconds - @last_update > @delay
+	end
+	def animate!
+		(@frame >= @states[@state].max - 1 ? animation_complete : @frame += 1)
+	end
+	def animation_complete
+		@frame = @states[@state].min
+	end
+end
+RGJ05.new.show
